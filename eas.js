43a606ba543411a37aa43b91c76420fe5f9ba4b1
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-25 19:15:29 -0800

    Upload files for testing purposes

--- eas.js
+++ eas.js
@@ -1,37 +1,60 @@
 const container = document.querySelector('.grid-container');
-
 const newGrid = document.querySelector('.new-grid');
 
-const SIZE = 257; //16 * 16 = 256 + 1
+const cell = container.children;
+
+const SIZE = 16; //16 * 16 = 256
 
 function createGrid(numDivs) {
-    for (let i = 1; i < numDivs; i++) { 
+    for (let i = 0; i < numDivs * numDivs; i++) { 
         const div = document.createElement('div');
         container.appendChild(div);
     }
 }
+
+function clear(elements) {
+    elements.style.backgroundColor = "transparent";
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
 
 createGrid(SIZE);
 
 function createNewGrid() {
     let squares = prompt("Number of squares per side: ");
 
+    let height = container.offsetHeight;
+
     if(squares > 100) {
         squares = 100;
+    } else while (squares < 1) {
+        alert("Cannot be less than 1!");
+        squares = prompt("Number of squares per side: ")
     }
 
     container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
 
-    let newSize = squares * squares + 1;
-    createGrid(newSize);
+    let squareSize = Math.round(height/squares);
+
+    for(let i = 0; i < cell.length; i++)
+    {
+        cell[i].style.height = `${squareSize}px`;
+        cell[i].style.width = `${squareSize}px`;
+    }
+
+    createGrid(squares);
+
 }
 
 container.addEventListener("mouseover", event => {
     event.target.style.backgroundColor = "purple";
-    console.log("mouse in");
 });
 
 newGrid.addEventListener('click', function() {
+    container.querySelectorAll('div').forEach(clear);
     createNewGrid();
 });
 
